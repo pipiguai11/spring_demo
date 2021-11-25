2e0d22c8809f849a2728fdcd4cc9d8b9d3deb0d3
linhw <linhw@dist.com.cn>
2021-11-25 11:08:42 +0800

    fix:添加邮件发送功能

--- src/test/java/com/lhw/spring/SpringDemoApplicationTests.java
+++ src/test/java/com/lhw/spring/SpringDemoApplicationTests.java
@@ -1,13 +1,42 @@
 package com.lhw.spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @SpringBootTest
 class SpringDemoApplicationTests {
 
+    @Autowired
+    JavaMailSenderImpl sender;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("953522392@qq.com");
+//        simpleMailMessage.setTo("linhw@dist.com.cn");
+        simpleMailMessage.setTo("623010541@qq.com");
+        simpleMailMessage.setSubject("hello world");
+        simpleMailMessage.setText("嗨，臭宝");
+        sender.send(simpleMailMessage);
+
+//        MimeMessage mimeMessage = sender.createMimeMessage();
+//        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+//        messageHelper.setFrom("953522392@qq.com");
+//        messageHelper.setTo("linhw@dist.com.cn");
+//        messageHelper.setSubject("Happy New Year");
+//        messageHelper.setText("新年快乐！");
+//        messageHelper.addInline("doge.gif", new File("E:\\temp\\1.jpg"));
+//        messageHelper.addAttachment("work.docx", new File("E:\\temp\\test.doc"));
+//        sender.send(mimeMessage);
+
     }
 
 }

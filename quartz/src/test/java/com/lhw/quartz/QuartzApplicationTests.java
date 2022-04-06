f6ba8b2b62510a9929c9dbf04494d0c082e734cd
linhw <953522392@qq.com>
2022-04-06 13:41:10 +0800

    fix(quartz)：测试将调度器信息、触发器信息、任务信息存放到数据库中

--- quartz/src/test/java/com/lhw/quartz/QuartzApplicationTests.java
+++ quartz/src/test/java/com/lhw/quartz/QuartzApplicationTests.java
@@ -1,13 +1,38 @@
 package com.lhw.quartz;
 
+import com.lhw.quartz.job.HelloJob2;
+import com.lhw.quartz.jobdetail.JobDetailHandler;
+import com.lhw.quartz.trigger.TriggerHandler;
 import org.junit.jupiter.api.Test;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QuartzApplicationTests {
 
+    @Autowired
+    private Scheduler scheduler;
+
     @Test
     void contextLoads() {
+
+        try {
+            scheduler.getContext().putIfAbsent("skey","mySchedulerTest");
+
+            Trigger trigger = TriggerHandler.createSimpleTrigger();
+            JobDetail jobDetail = JobDetailHandler.createJobDetail(HelloJob2.class);
+
+            scheduler.scheduleJob(jobDetail,trigger);
+            scheduler.start();
+
+        }catch (SchedulerException e){
+            e.printStackTrace();
+        }
+
     }
 
 }

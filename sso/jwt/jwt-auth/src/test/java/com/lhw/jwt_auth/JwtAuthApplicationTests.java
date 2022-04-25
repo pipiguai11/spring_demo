daa8c3d741284c027a06e3f68d1c98b8b6109757
linhw <953522392@qq.com>
2022-04-25 15:55:00 +0800

    fix(spring-starter)：修改maven的打包plugin配置

--- sso/jwt/jwt-auth/src/test/java/com/lhw/jwt_auth/JwtAuthApplicationTests.java
+++ sso/jwt/jwt-auth/src/test/java/com/lhw/jwt_auth/JwtAuthApplicationTests.java
@@ -1,13 +1,13 @@
-package com.lhw.jwt_auth;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JwtAuthApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.lhw.jwt_auth;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JwtAuthApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

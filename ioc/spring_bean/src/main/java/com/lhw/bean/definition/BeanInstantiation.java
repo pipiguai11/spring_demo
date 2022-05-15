84098b3244113bde0251baf49e3a6147a31cb1fb
linhw <953522392@qq.com>
2022-05-15 11:23:47 +0800

    fix(spring_bean): 通过FactoryBean实例化对象

---
+++ ioc/spring_bean/src/main/java/com/lhw/bean/definition/BeanInstantiation.java
@@ -0,0 +1,27 @@
+package com.lhw.bean.definition;
+
+import com.lhw.bean.factory.UserFactoryBean;
+import com.lhw.container.domain.User;
+import org.springframework.context.support.GenericApplicationContext;
+
+/**
+ * @author ：linhw
+ * @date ：22.5.15 10:48
+ * @description：Bean实例化
+ * @modified By：
+ */
+public class BeanInstantiation {
+
+    public static void main(String[] args) {
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean( "userFactory",UserFactoryBean.class);
+
+        applicationContext.refresh();
+
+        User user = applicationContext.getBean("userFactory", User.class);
+        System.out.println(user);
+
+        applicationContext.close();
+    }
+
+}
--- ioc/spring_bean/src/test/java/com/lhw/bean/SpringBeanApplicationTests.java
+++ ioc/spring_bean/src/test/java/com/lhw/bean/SpringBeanApplicationTests.java
@@ -1,5 +1,8 @@
 package com.lhw.bean;
 
+import com.lhw.bean.factory.UserFactoryBean;
+import com.lhw.bean.utils.ApplicationContextUtil;
+import com.lhw.container.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SpringBeanApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        //将FactoryBean对象注册到容器中，并通过它获取对应的Bean实例
+        ApplicationContextUtil.registerBean("userFactoryBean", UserFactoryBean.class);
+        System.out.println(ApplicationContextUtil.getBean("userFactoryBean", User.class));
     }
 
 }

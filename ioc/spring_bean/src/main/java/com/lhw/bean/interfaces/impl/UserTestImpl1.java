a5e129ac7a5e774f4ea4446abbb01dfedbff9778
linhw <953522392@qq.com>
2022-05-15 12:46:47 +0800

    fix(spring_bean): 使用InitializingBean对Bean进行初始化

--- ioc/spring_bean/src/main/java/com/lhw/bean/interfaces/impl/UserTestImpl1.java
+++ ioc/spring_bean/src/main/java/com/lhw/bean/interfaces/impl/UserTestImpl1.java
@@ -1,6 +1,7 @@
 package com.lhw.bean.interfaces.impl;
 
 import com.lhw.bean.interfaces.UserTestInterface;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  * @author ：linhw
@@ -8,9 +9,18 @@ import com.lhw.bean.interfaces.UserTestInterface;
  * @description：测试实现类1
  * @modified By：
  */
-public class UserTestImpl1 implements UserTestInterface {
+public class UserTestImpl1 implements UserTestInterface, InitializingBean {
     @Override
     public String getMessage() {
         return "测试实现类1";
+    }
+
+    /**
+     * 只有在IOC容器中创建的对象才会调用到这个方法
+     * @throws Exception
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("UserTestImpl1调用了InitializingBean#afterPropertiesSet 进行Bean的初始化");
     }
 }
